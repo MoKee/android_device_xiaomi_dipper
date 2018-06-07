77e80c8d49932666a532b02530a7e4423ddff356
Bruno Martins <bgcngm@gmail.com>
2018-06-07 09:55:21 +0100

    dipper: Reset to system only device tree
    Change-Id: Ibb7927b8a814fd6e65cc700df68e18920f8f9ef6

--- device.mk
+++ device.mk
@@ -1,31 +1,25 @@
 #
-# Copyright (C) 2018 The MoKee Project
+# Copyright (C) 2018 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-# Overlay
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/xiaomi/dipper/dipper-vendor.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2248
+TARGET_SCREEN_WIDTH := 1080
+
+# Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-mokee
+    $(LOCAL_PATH)/overlay
 
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Inherit from sdm845-common
 $(call inherit-product, device/xiaomi/sdm845-common/sdm845.mk)
-
-# Call the proprietary setup
-$(call inherit-product, vendor/xiaomi/dipper/dipper-vendor.mk)
--- mokee.mk
+++ mokee.mk
@@ -1,20 +1,29 @@
-$(call inherit-product, device/xiaomi/dipper/full_dipper.mk)
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/dipper/device.mk)
 
 # Inherit some common MK stuff.
 $(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
+# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := mk_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := MI 8
+PRODUCT_MODEL := Mi 8
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.11.0.OEACNFA:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="dipper-user 8.0.0 OPR1.170623.032 V9.5.18.0.ODGCNFA release-keys"
+    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEACNFA release-keys" \
+    PRODUCT_NAME="dipper" \
+    TARGET_DEVICE="dipper"
 
-BUILD_FINGERPRINT := Xiaomi/dipper/dipper:8.0.0/OPR1.170623.032/V9.5.18.0.ODGCNFA:user/release-keys
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.mk.maintainer=wood
