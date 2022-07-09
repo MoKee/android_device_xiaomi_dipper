0074bdb4db0efd3094d3b0c2f269deeaf1a88efc
Martincz Gao <martincz@mokeedev.com>
2022-07-09 14:42:40 +0800

    dipper: mokee-ify

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-mokee
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
@@ -82,7 +82,7 @@ PRODUCT_COPY_FILES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.xiaomi_sdm845
+    vendor.mokee.livedisplay@2.0-service.xiaomi_sdm845
 
 # NFC
 PRODUCT_PACKAGES += \
--- mokee_dipper.mk
+++ mokee_dipper.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/xiaomi/dipper/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common MoKee stuff.
+$(call inherit-product, vendor/mokee/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_dipper
+PRODUCT_NAME := mokee_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
